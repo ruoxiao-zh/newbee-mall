d397438ef4c3b41014ffb0717adb511a486fb6c9
zhangruoxiao <zhangruoxiao@doumi.com>
2021-04-20 18:44:35 +0800

    init project

---
+++ src/test/java/Itd/newbee/mall/newbeemall/NewbeeMallApplicationTests.java
@@ -0,0 +1,13 @@
+package Itd.newbee.mall.newbeemall;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NewbeeMallApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
